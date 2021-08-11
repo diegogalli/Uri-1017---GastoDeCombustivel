c869c2a6286ab2c1b2a33cbd435f5d483ff3da79
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:21:00 -0300

    Uri 1017 Gasto de combustivel

---
+++ GastoCombustivel/GastoCombustivel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace GastoCombustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int TempoGasto, VelMedia, distancia;
+            double litros;
+
+            TempoGasto = int.Parse(Console.ReadLine());
+            VelMedia = int.Parse(Console.ReadLine());
+
+            distancia = TempoGasto * VelMedia;
+
+            litros = distancia / (double) 12;
+
+            Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
